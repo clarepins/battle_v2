ef5f40c90989d8f69c24ec15baed21e574c492c0
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-20 15:11:21 +0000

    changed post request to get request using sinatra session

--- spec/features/capybara_feature_spec.rb
+++ spec/features/capybara_feature_spec.rb
@@ -4,6 +4,6 @@ feature 'Names' do
     fill_in(:player_1, with: 'Anu')
     fill_in(:player_2, with: 'Taz')
     click_button 'Submit'
-    expect(page).to have_content 'Anu vs Taz'
+    expect(page).to have_content 'Anu Vs Taz'
   end
 end
