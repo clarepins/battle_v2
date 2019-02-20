2abf731627640b901bc2908d57cfdc0e89acc470
anu <anu_iv2002@hotmail.com>
2019-02-20 15:23:41 +0000

    knowing the player 2 points

--- spec/features/capybara_feature_spec.rb
+++ spec/features/capybara_feature_spec.rb
@@ -7,3 +7,13 @@ feature 'Names' do
     expect(page).to have_content 'Anu Vs Taz'
   end
 end
+
+feature 'Hit points' do
+  scenario 'Player 2 hit points' do
+    visit('/')
+    fill_in(:player_1, with: 'Anu')
+    fill_in(:player_2, with: 'Taz')
+    click_button 'Submit'
+    expect(page).to have_content 'Taz: 60HP'
+  end
+end
